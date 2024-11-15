1d74e1067b50b4f43e9776abb30f29fe73059aad
HungHoang1 <2051063705@e.tlu.edu.vngit config --global user.email 2051063705@e.tlu.edu.vngit config --global user.email 2051063705@e.tlu.edu.vn>
2024-11-15 11:34:06 +0700

    demo

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,51 @@
+import logo from './logo.png'
+import header_img from './header_img.png'
+import pho from './pho.png'
+import bundau from './bundau.png'
+import banhcuon from './banhcuon.png'
+import buncha from './buncha.png'
+import bunthang from './bunthang.png'
+import bunrieu from './bunrieu.png'
+import xoikhuc from './xoikhuc.png'
+import cross_icon from './cross_icon.png'
+import search_icon from './search_icon.png'
+
+
+export const assets = {
+    logo,
+    header_img,
+    cross_icon,
+    search_icon,
+}
+
+export const menu_list = [
+    {
+        menu_name: "Phở",
+        menu_image: pho
+    },
+    {
+        menu_name: "Bún Đậu",
+        menu_image: bundau
+    },
+    {
+        menu_name: "Bánh Cuốn",
+        menu_image: banhcuon
+    },
+    {
+        menu_name: "Bún Chả",
+        menu_image: buncha
+    },
+    {
+        menu_name: "Bún Riêu",
+        menu_image: bunrieu
+    },
+    {
+        menu_name: "Bún Thang",
+        menu_image: bunthang
+    },
+    {
+        menu_name: "Xôi Khúc",
+        menu_image: xoikhuc
+    },
+    
+]
